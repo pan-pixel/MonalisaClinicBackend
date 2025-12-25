30bce906402fe61217cbc38f477030ac746ec45e
pan-pixel <prathamarora333@gmail.com>
2025-12-26 02:47:04 +0530

    CORS fixed

--- monalisa_backend/settings.py
+++ monalisa_backend/settings.py
@@ -179,6 +179,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8090",
     "http://127.0.0.1:8090",
+    "https://monalisaclinic.com",
+    "https://monalisaclinic.up.railway.app",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = DEBUG  # Only for development
